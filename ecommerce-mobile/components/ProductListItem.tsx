14a5ae8c90015802a852217ad067fc5faab6e50c
Robert <rob76c@gmail.com>
2024-11-25 23:47:50 -0500

    Product details screen

--- ecommerce-mobile/components/ProductListItem.tsx
+++ ecommerce-mobile/components/ProductListItem.tsx
@@ -1,4 +1,4 @@
-import { Text } from "react-native";
+import { Pressable, Text } from "react-native";
 import {
     Button,
     ButtonText,
@@ -11,6 +11,7 @@ import {
   import { VStack } from "@/components/ui/vstack";
   import { Heading } from "@/components/ui/heading";
   import { Box } from "@/components/ui/box";
+import { Link } from "expo-router";
 
 
   
@@ -18,7 +19,10 @@ import {
 
 export default function ProductListItem ({product}) {
     return (
+        <Link href={`/product/${product.id}`} asChild>
+            <Pressable className="flex-1">
         <Card className="p-5 rounded-lg max-w-[360px] flex-1">
+
       <Image
         source={{
           uri: product.image,
@@ -30,27 +34,12 @@ export default function ProductListItem ({product}) {
       <Text className="text-sm font-normal mb-2 text-typography-700">
         {product.name}
       </Text>
-      <VStack className="mb-6">
+    
         <Heading size="md" className="mb-4">
           ${product.price}
         </Heading>
-        <Text size="sm">
-          {product.description}
-        </Text>
-      </VStack>
-      <Box className="flex-col sm:flex-row">
-        <Button className="px-4 py-2 mr-0 mb-3 sm:mr-3 sm:mb-0 sm:flex-1">
-          <ButtonText size="sm">Add to cart</ButtonText>
-        </Button>
-        <Button
-          variant="outline"
-          className="px-4 py-2 border-outline-300 sm:flex-1"
-        >
-          <ButtonText size="sm" className="text-typography-600">
-            Wishlist
-          </ButtonText>
-        </Button>
-      </Box>
     </Card>
+    </Pressable>
+    </Link>
     )
 }
